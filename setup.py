916399e55c3711e997ea6f0fa94bd0c9bd308922
S.R. Shuva Dev <s.r.shuvadeb@gmail.com>
2026-02-20 20:57:06 +0600

    project setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Shuva Dev',
+    author_email= 's.r.shuvadeb@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
